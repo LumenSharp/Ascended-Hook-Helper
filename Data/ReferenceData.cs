ac4657f8a5ef62be170bea48ed74d10522d2c064
LumenSharp <103589496+LumenSharp@users.noreply.github.com>
2025-06-07 23:09:55 -0700

    Local Caching & DebounceHelper.cs

--- Data/ReferenceData.cs
+++ Data/ReferenceData.cs
@@ -5,7 +5,12 @@ public static class ReferenceData
     public const string RawGitHubAsaApiRepoUrl = "https://raw.githubusercontent.com/ServersHub/ServerAPI/main";
     public const string GitHubAsaApiHeadersFilePath = "/AsaApi/Core/Public/API/ARK";
 
-    public static List<string> ArkHeaderNamesList = new List<string>()
+    public const string SaveKey_HeaderDataPrefix = "header_data_";
+    public const string SaveKey_HeaderStructDatasList = "header_struct_datas_list";
+    public const string SaveKey_HeadersFunctionDatasList = "header_function_datas_list";
+    public const string SaveKey_LastHeadersCachedDateTime = "last_headers_cached_datetime";
+
+    public static readonly List<string> ArkHeaderNamesList = new List<string>()
     {
         "Actor",
         "GameMode",
---
+++ Helpers/DebounceHelper.cs
@@ -0,0 +1,24 @@
+﻿namespace AscendedHookHelper.Helpers;
+
+public class DebounceHelper
+{
+    private CancellationTokenSource? _debounceCancellationToken = null;
+    public async Task DebounceAsync(Func<CancellationToken, Task> func, int debounceMilliseconds = 500)
+    {
+        try
+        {
+            if (_debounceCancellationToken != null)
+                _debounceCancellationToken.Cancel();
+
+            _debounceCancellationToken = new CancellationTokenSource();
+
+            await Task.Delay(debounceMilliseconds, _debounceCancellationToken.Token);
+
+            if (_debounceCancellationToken.IsCancellationRequested)
+                return;
+
+            await func(_debounceCancellationToken.Token);
+        }
+        catch (TaskCanceledException) { }
+    }
+}
